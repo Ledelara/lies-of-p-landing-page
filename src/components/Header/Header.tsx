339491502786118843d27c89cac07e75bcf8f4a7
Leandro de Lara Celio <122060742+Ledelara@users.noreply.github.com>
2025-03-22 17:57:26 -0300

    ✨ feat: componente Header

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,32 @@
+'use client';
+
+import Image from "next/image";
+import '../../styles/components/_header.scss'
+import { useRouter } from "next/navigation";
+
+export default function Header() {
+
+  const navigate = useRouter();
+  
+  return (
+    <header className="header">
+      <ul className="header__menu">
+        <li className="header__menu__item">
+          <a className="header_menu_text" onClick={() => navigate.push('/')}>Home</a>
+        </li>
+        <li className="header__menu__item">
+          <a className="header_menu_text" href="https://lnkfi.re/LiesofP">Música</a>
+        </li>
+        <li className="header__menu__image">
+          <Image src="/images/lies-of-p-logo.png" alt="logo" width={100} height={100} />
+        </li>
+        <li className="header__menu__item">
+          <a className="header_menu_text" href="https://gear.liesofp.com/">Loja</a>
+        </li>
+        <li className="header__menu__item">
+          <a className="header_menu_text" href="https://store.steampowered.com/app/1627720/Lies_of_P/">Compre agora</a>
+        </li>
+      </ul>
+    </header>
+  )
+}
