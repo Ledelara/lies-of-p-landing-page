759762d6ed3c3e50de624cf7f51c545e9bc5e74d
Leandro de Lara Celio <122060742+Ledelara@users.noreply.github.com>
2025-03-22 17:59:28 -0300

    ♻️ refactor: padronizando redirecionamento

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -15,16 +15,16 @@ export default function Header() {
           <a className="header_menu_text" onClick={() => navigate.push('/')}>Home</a>
         </li>
         <li className="header__menu__item">
-          <a className="header_menu_text" href="https://lnkfi.re/LiesofP">Música</a>
+          <a className="header_menu_text" onClick={() => navigate.push('https://lnkfi.re/LiesofP')}>Música</a>
         </li>
         <li className="header__menu__image">
           <Image src="/images/lies-of-p-logo.png" alt="logo" width={100} height={100} />
         </li>
         <li className="header__menu__item">
-          <a className="header_menu_text" href="https://gear.liesofp.com/">Loja</a>
+          <a className="header_menu_text" onClick={() => navigate.push('https://gear.liesofp.com/')}>Loja</a>
         </li>
         <li className="header__menu__item">
-          <a className="header_menu_text" href="https://store.steampowered.com/app/1627720/Lies_of_P/">Compre agora</a>
+          <a className="header_menu_text" onClick={() => navigate.push('https://store.steampowered.com/app/1627720/Lies_of_P/')}>Compre agora</a>
         </li>
       </ul>
     </header>
